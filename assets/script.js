aaf96eac6fb3f138aa9c36a90b0e4ce9170fa009
Woooooood <wassim.boulatika@supinternet.fr>
2019-12-02 15:42:33 +0100

    added mew and scalling animation when ecounter him, added city to background

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 window.onload= function (){
-    var dragonite = document.querySelector('#dragonite')
-    dragonite.setAttribute("animation-mixer","clip: Idle");
-    console.log(dragonite)
-}
+    let camera = document.querySelector('a-camera');
+    let mew = document.querySelector("#mew-entity");
+    console.log(mew);
+
+    AFRAME.registerComponent('listener', {
+        tick: function () {
+            if(this.el.getAttribute('position').z <= -12.7){
+                return mew.setAttribute("animation", "property: scale; to: 0.004 0.004 0.004; dur: 1000;");
+                console.log("gerfghbn")
+               
+            }
+        }
+    });
+
+
+    
+};
 
